8549a218e1607e98558df5b665edf370d33ae83a
jonwelch888 <jon.welch@g.austincc.edu>
2024-07-08 00:23:28 -0500

    1st attempt at creating the main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
-/* 
- * your comment header here
- */
+/* **************************************
+Name: Jon Welch
+Assignment: 7
+Purpose of the file: This source file contains the main function for testing the HashTable class.
+It generates test data, inserts it into the hash table, and demonstrates various operations.
+
+@note This file is associated with [main.h]
+;*************************************** */
 
 #include "main.h"
 
-int main() {
+int main()
+{
     //seed the rand function
     srand(time(NULL));
 
@@ -47,19 +53,72 @@ int main() {
      * it works.
      */
     
-    // create your hash table object here
+    // Create your hash table object here
+    HashTable ht;
     
-    // show it is empty by calling getCount and printTable
-    
-    // try and put ALL the test data into the table and show what happens
+    // Show it is empty by calling getCount and printTable
+    cout << "Initial hash table state:" << endl;
+    cout << "Entry count: " << ht.getCount() << endl;
+    ht.printTable();
+    cout << endl;
 
-    // continue using and testing your table, add and remove data,
-    // do whatever it takes to full test your object and prove it
-    // is robust and can handle all use cases.
+    // Try and put ALL the test data into the table and show what happens
+    cout << "Inserting test data into hash table..." << endl;
+    for (int i = 0; i < (testdatasize); i++)
+    {
+        if (ht.insertEntry(ids[i], &strs[i]))
+        {
+            cout << "Inserted: " << ids[i] << " : " << strs[i] << endl;
+        }
+        else
+        {
+            cout << "Failed to insert: " << ids[i] << " : " << strs[i] << endl;
+        }
+    }
+    cout << endl;
 
-    
-    
-    
-    
+    // Show the state of the hash table after insertion
+    cout << "Hash table after insertion:" << endl;
+    cout << "Entry count: " << ht.getCount() << endl;
+    ht.printTable();
+    cout << endl;
+
+    // Demonstrate retrieval of data
+    cout << "Retrieving data from hash table:" << endl;
+    for (int i = 0; i < (testdatasize); i++)
+    {
+        string data = ht.getData(ids[i]);
+        if (!data.empty())
+        {
+            cout << "Retrieved: ID " << ids[i] << " -> " << data << endl;
+        }
+        else
+        {
+            cout << "ID " << ids[i] << " not found in hash table." << endl;
+        }
+    }
+    cout << endl;
+
+    // Demonstrate removal of data
+    cout << "Removing entries from hash table:" << endl;
+    for (int i = 0; i < (testdatasize); i++)
+    {
+        if (ht.removeEntry(ids[i]))
+        {
+            cout << "Removed: ID " << ids[i] << endl;
+        }
+        else
+        {
+            cout << "Failed to remove: ID " << ids[i] << endl;
+        }
+    }
+    cout << endl;
+
+    // Show the state of the hash table after removal
+    cout << "Hash table after removal:" << endl;
+    cout << "Entry count: " << ht.getCount() << endl;
+    ht.printTable();
+    cout << endl;
+
     return 0;
 }
