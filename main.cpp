aa2992c142338640d3e9f6efc1c1c3710c664573
jonwelch888 <jon.welch@g.austincc.edu>
2024-07-09 00:39:04 -0500

    cleaning up the comments

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 /* **************************************
-Name: Jon Welch
-Assignment: 7
-Purpose of the file: This source file contains the main function for testing the HashTable class.
-It generates test data, inserts it into the hash table, and demonstrates various operations.
-
-@note This file is associated with [main.h]
+* Name: Jon Welch
+* Assignment: 7
+* Purpose of the file: This source file contains the main function for testing the HashTable class.
+* It generates test data, inserts it into the hash table, and demonstrates various operations.
+* 
+* @note This file is associated with [main.h]
 ;*************************************** */
 
 #include "main.h"
