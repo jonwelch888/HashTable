10509e966901c54cb5428c60e83808ff8ce22133
jonwelch888 <jon.welch@g.austincc.edu>
2024-07-08 23:46:29 -0500

    attempting to clean up the code.
    fix the comments and remove parameters within the prototypes

--- hashtable.h
+++ hashtable.h
@@ -1,13 +1,15 @@
-/* **************************************
-Name: Jon Welch
-Assignment: 7
-Purpose of the file: This header file declares the HashTable class,
-which uses separate chaining to handle collisions. It supports operations 
-such as inserting, deleting, and retrieving entries, as well as printing the table 
-and checking the number of entries.
 
-@note file is associated with [hashtable.cpp] & [linkedlist.h];
-*************************************** */
+
+
+/**************************
+* Name: Jon Welch
+* Assignment: 7
+* Purpose of the file: This header file declares the HashTable class,
+* which uses separate chaining to handle collisions. It supports operations 
+* such as inserting, deleting, and retrieving entries, as well as printing the table 
+* and checking the number of entries.
+* @note file is associated with [hashtable.cpp] & [linkedlist.h];
+* ***************************/
 
 #ifndef HASHTABLE_H
 #define HASHTABLE_H
@@ -31,33 +33,28 @@ class HashTable
     /* *************************************
     Constructor and Destructor
     ************************************* */
-    HashTable();  // Constructor to initialize the hash table
-    ~HashTable();  // Destructor to clean up allocated memory
+    HashTable(); 
+    ~HashTable();  
 
-    /* *************************************
-    Accessors:
-    bool insertEntry(int id, string* data); Inserts a new entry into the hash table
-    string getData(int id); Retrieves the data associated with the given id
-    bool removeEntry(int id); Removes the entry with the given id from the hash table
-    int getCount(); Returns the number of entries in the hash table
-    void printTable(); Prints the contents of the hash table
-    ************************************* */
-
-    bool insertEntry(int id, std::string* data);  // Inserts an entry into the hash table
-    std::string getData(int id);  // Retrieves the data for the given id
-    bool removeEntry(int id);  // Removes the entry with the given id
-    int getCount();  // Returns the number of entries in the hash table
-    void printTable();  // Prints the hash table
+    /**********************
+     Getters/Accessors
+    **********************/
+    bool insertEntry(int id, std::string* data);  
+    std::string getData(int id);  
+    bool removeEntry(int id); 
+    int getCount();  
+    void printTable();  
 
     private:
-    /* *******************************************
+    /********************************************
     Private methods:
     int hash(int id); Generates the hash for a given id
-    ********************************************* */
-    int hash(int id);  // Hash function
+    LinkedList table[HASHTABLESIZE]; Array of linked lists
+    **********************************************/
+    int hash(int id); 
 
-    LinkedList table[HASHTABLESIZE];  // Array of linked lists
-    int count;  // Number of entries in the hash table
+    LinkedList table[HASHTABLESIZE];  
+    int count; 
 };
 
 #endif // HASHTABLE_H
