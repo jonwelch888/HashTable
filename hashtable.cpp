63e120c1f78b622ba9003a6de5c68b805bd8f935
jonwelch888 <jon.welch@g.austincc.edu>
2024-07-09 20:35:01 -0500

    Trying to troubleshoot
    (index != -1)  to addnode

--- hashtable.cpp
+++ hashtable.cpp
@@ -44,7 +44,7 @@ bool HashTable::insertEntry(int id, std::string* data)
     @return : true if the insertion was successful, false otherwise.
     ************************************* */
     int index = hash(id);
-    bool success = table[index].addNode(id, data);
+    bool success = (index != -1) && table[index].addNode(id, data); // Trying addNode();
     count += success ? 1 : 0;
     return success;
 }
